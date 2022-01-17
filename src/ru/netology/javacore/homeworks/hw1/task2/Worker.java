72b1b3d24f9c7339ecd7965f366472acfd8282a3
Alisa Kalashnikova <alisa.kalashnikova@relex.fi>
2022-01-17 14:31:28 +0200

    Hm task 1.2

---
+++ src/ru/netology/javacore/homeworks/hw1/task2/Worker.java
@@ -0,0 +1,32 @@
+package ru.netology.javacore.homeworks.hw1.task2;
+
+public class Worker {
+    private OnTaskDoneListener callback;
+    private OnTaskErrorListener errorCallback;
+
+    public Worker(OnTaskDoneListener callback, OnTaskErrorListener errorCallback) {
+        this.callback = callback;
+        this.errorCallback = errorCallback;
+    }
+
+    @FunctionalInterface
+    public interface OnTaskDoneListener {
+        void onDone(String result);
+    }
+
+    @FunctionalInterface
+    public interface OnTaskErrorListener {
+        void onError(String result);
+    }
+
+    public void start() {
+        for (int i = 0; i < 100; i++) {
+            if (i == 33) {
+                errorCallback.onError("Task " + i + " is completed with an error");
+                continue;
+            }
+            callback.onDone("Task " + i + " is done");
+        }
+    }
+
+}
